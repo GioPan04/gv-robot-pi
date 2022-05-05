911abf979b0d133dc9becd1db1b9e3f384ead3d2
GioPan04 <gioele@pannetto.com>
2022-05-05 22:18:42 +0200

    Created Motor class

---
+++ GPIO/Motor.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO  # type: ignore
+
+class Motor:
+  def __init__(self, dir_pin: int, hrz = 500) -> None:
+    self.dir_pin = dir_pin
+    GPIO.setup(dir_pin, GPIO.OUT)
+    self.pwm = GPIO.PWM(dir_pin, hrz)
+  
+  def start(self) -> None:
+    self.pwm.start(50)
+  
+  def change_speed(self, hrz) -> None:
+    self.pwm.ChangeFrequency(hrz)
+    
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from GPIO.Motor import Motor
 import RPi.GPIO as GPIO  # type: ignore
 from GPIO.Ultrasonic import Ultrasonic
 from helpers import calculate_speed
@@ -15,12 +16,10 @@ import config
 # Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(config.MOTOR_LEFT_PIN, GPIO.OUT)
-GPIO.setup(config.MOTOR_RIGHT_PIN, GPIO.OUT)
 
 # Initialize external devices
-motorL = GPIO.PWM(config.MOTOR_LEFT_PIN, 500)
-motorR = GPIO.PWM(config.MOTOR_RIGHT_PIN, 500)
+motorL = Motor(config.MOTOR_LEFT_PIN)
+motorR = Motor(config.MOTOR_RIGHT_PIN)
 sensorT = Ultrasonic(config.SONIC_TOP_TRG_PIN, config.SONIC_TOP_ECH_PIN)
 sensorM = Ultrasonic(config.SONIC_MDL_TRG_PIN, config.SONIC_MDL_ECH_PIN)
 sensorB = Ultrasonic(config.SONIC_BTM_TRG_PIN, config.SONIC_BTM_ECH_PIN)
@@ -32,16 +31,16 @@ color_thread = ColorThread("Colors")
 if __name__ == '__main__':
   distance_thread.start()
   color_thread.start()
-  motorL.start(50)
-  motorR.start(50)
+  motorL.start()
+  motorR.start()
 
   # Run endlessly the motors, adjust the left and right speed by it's distance from the left wall
   try:
     while True:
       distance = distance_thread.distance
       (left, right) = calculate_speed(distance)
-      motorL.ChangeFrequency(left)
-      motorR.ChangeFrequency(right)
+      motorL.change_speed(left)
+      motorR.change_speed(right)
 
       # print(f"Left: {left}Hz Right: {right}Hz Distance: {distance}cm")
   except KeyboardInterrupt: # Don't log ^C
